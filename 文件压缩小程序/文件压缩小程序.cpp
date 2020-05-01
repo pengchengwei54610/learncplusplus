d599997b34666391abb14829fea3ff81ba32db47
pengchengwei54610 <1375767307@qq.com>
2020-05-01 14:08:38 +0800

    2020/5/1 14:08更新

--- 文件压缩小程序/文件压缩小程序.cpp
+++ 文件压缩小程序/文件压缩小程序.cpp
@@ -1,56 +1,283 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+/*通过Huffman编码对文件进行无损压缩,对ser.log压缩一次至6.43MB，压缩比约为65%，多次压缩可有更高压缩率*/
+#define _CRT_SECURE_NO_WARNINGS 
+#include <algorithm>
+#include <map>
+#include <iostream> 
+#include <cstring>
+#include <fstream>
+#include <cstdlib> 
+#include <cstdio> 
+#include <iomanip>
+#include <string>
+#include <vector>
 using namespace std;
 
-void replaceString(string& origin, string old_value, string new_value) {
+//数据结构定义
+//Huffman树节点数据结构定义
+struct Node
+{
+	char c; //字符
+	int weight; //权重
+	Node* left; //左子节点
+	Node* right; //右子节点
+	Node* parent; //父节点
+};
 
-    /* 该函数用于对origin全文查找出现的old_value 并用new_value替换 */
-
-    for (string::size_type pos(0); pos != string::npos; pos += new_value.length()) {
-        if ((pos = origin.find(old_value, pos)) != string::npos)
-            origin.replace(pos, old_value.length(), new_value);
-        else break;
-    }
+//统计文件中各字符的出现次数及文件总字节数
+int countChar(ifstream& fin, vector<int>& count)
+{
+	char c;
+	fin.read(&c, 1);
+	while (!fin.eof())
+	{
+		++count[unsigned char(c)];
+		fin.read(&c, 1);
+	}
+	int sum = 0;
+	for (auto i : count)
+		sum += i;
+	return sum;
 }
 
-int main(int argc, char* argv[]) {
-    cout << "Zipper 0.001! Author: root" << endl;
-    if (argc != 4) {
-        cerr << "Please make sure the number of parameters is correct." << endl;
-        return -1;
-    }
+//构造Huffman树的节点
+void createNodes(vector<Node>& nodes, const vector<int>& count)
+{
+	for (int i = 0; i < 256; ++i)
+		if (count[i])
+		{
+			Node node = { char(i),count[i],NULL,NULL,NULL };
+			nodes.push_back(node);
+		}
+}
 
-    if (strcmp(argv[3], "zip")) {
-        cerr << "Unknown parameter!\nCommand list:\nzip" << endl;
-        return -1;
-    }
+//将通过权重节点排序
+bool cmp(Node a, Node b)
+{
+	return a.weight > b.weight;
+}
 
-    ifstream fin(argv[1], ios::binary);               // 以二进制方式打开文件
-    if (!fin) {
-        cerr << "Can not open the input file!" << endl;   // 输出错误信息并退出
-        return -1;
-    }
+//构造Huffman树
+Node* createTree(vector<Node>& nodes)
+{
+	sort(nodes.begin(), nodes.end(), cmp);
+	while (nodes.size() != 1)
+	{
+		Node* leftNode = new Node, * rightNode = new Node;
+		*leftNode = nodes.back();
+		nodes.pop_back();
+		*rightNode = nodes.back();
+		nodes.pop_back();
+		Node parentNode = { 0,leftNode->weight + rightNode->weight,leftNode,rightNode,NULL };
+		leftNode->parent = &parentNode;
+		rightNode->parent = &parentNode;
+		nodes.push_back(parentNode);
+		sort(nodes.begin(), nodes.end(), cmp);
+	}
+	Node* root = new Node;
+	*root = nodes.back();
+	nodes.pop_back();
+	return root;
+}
 
-    istreambuf_iterator<char> beg(fin), end;              // 设置两个文件指针，指向开始和结束，以char(一字节)为步长
-    string content(beg, end);                             // 将文件全部读入string字符串
-    fin.close();                                          // 操作完文件后关闭文件句柄是一个好习惯
+//生成编码
+void generateCodes(map<char, string>& codes, Node* root, string code)
+{
+	if (root->left == 0 && root->right == 0)
+	{
+		codes[root->c] = code;
+		return;
+	}
+	generateCodes(codes, root->left, code + '0');
+	generateCodes(codes, root->right, code + '1');
+}
 
-    /* 在这里对冗余数据进行剔除 */
-    replaceString(content, "#Software: Hllpoj Server 1.0.1 / Logger 1.0.0 built 0001\r\n#Version: 1.0\r\n#Date: 2019-03-13 ", "");
-    replaceString(content, "#Fields: date time s-ip cs-method cs-uri-stem cs-uri-query s-port "
-        "cs-username c-ip cs(User-Agent) cs(Referer) sc-status sc-substatus sc-win32-status time-taken", "");
-    replaceString(content, "Mozilla/5.0+(Windows+NT+10.0;+Win64;+x64)+AppleWebKit/537.36+(KHTML,+like+Gecko)+", "");
-    replaceString(content, "2019-03-13 ", "");
+//将霍夫曼树和压缩后的内容写入压缩文件
+void compress(ifstream& fin, ofstream& fout, const int sum, const vector<int>& count, map<char, string>& codes)
+{
+	//写入原文件总字节数,字符总类数和各字符出现的次数
+	fout << sum << endl;
+	fout << codes.size() << endl;
+	for (int i = 0; i < 256; ++i)
+		if (count[i])
+			fout << i << ' ' << count[i] << endl;
+	//写入原文件内容
+	int pos = 0;
+	char input = 0, output = 0;
+	fin.clear();
+	fin.seekg(0, ios::beg);
+	fin.read(&input, 1);
+	while (!fin.eof())
+	{
+		string code = codes[input];
+		int codeSize = code.size();
+		for (int i = 0; i < codeSize; ++i)
+		{
+			output <<= 1;
+			if (code[i] == '1')
+				output |= 1;
+			else
+				output |= 0;
+			++pos;
+			if (pos == 8)
+			{
+				fout.write(&output, 1);
+				output = pos = 0;
+			}
+		}
+		fin.read(&input, 1);
+	}
+	if (pos)
+	{
+		output <<= (8 - pos);
+		fout.write(&output, 1);
+	}
+}
 
-    ofstream fout(argv[2], ios::binary); // 打开输出文件
-    if (!fout) {
-        cerr << "Can not open the output file!" << endl;
-        return -1;
-    }
-    fout << content;                                      // 直接将操作好的字符串进行输出
-    fout.close();
+//释放Huffman树
+void deleteTree(Node* root)
+{
+	if (root->left)
+		deleteTree(root->left);
+	if (root->right)
+		deleteTree(root->right);
+	delete root;
+}
 
-    cout << "Complete!" << endl;
-    return 0;
+//压缩总函数
+void zip(ifstream& fin, ofstream& fout)
+{
+	//统计文件中各字符出现次数及总字节数
+	vector<int> count(256, 0);
+	int sum = countChar(fin, count);
+	//构造Huffman树的节点
+	vector<Node> nodes;
+	createNodes(nodes, count);
+	//构造Huffman树
+	Node* root = createTree(nodes);
+	//生成Huffman编码
+	map<char, string> codes;
+	string code;
+	generateCodes(codes, root, code);
+	//写入文件
+	compress(fin, fout, sum, count, codes);
+	//释放Huffman树
+	deleteTree(root);
+}
+
+//读取原文件总字节数和各字符出现的次数
+int readCount(ifstream& fin, vector<int>& count)
+{
+	int sum = 0, codesSize = 0;
+	fin >> sum >> codesSize;
+	for (int i = 0; i < codesSize; ++i)
+	{
+		int c, times;
+		fin >> c >> times;
+		count[c] = times;
+	}
+	fin.get(); //读取最后一个\n
+	return sum;
+}
+
+//写入解压文件
+void uncompress(ifstream& fin, ofstream& fout, int sum, Node* const root)
+{
+	//将压缩文件内容转换为Huffman编码
+	string content;
+	char c;
+	fin.read(&c, 1);
+	while (!fin.eof())
+	{
+		for (int i = 0; i < 8; ++i)
+		{
+			if (c & (1 << 7))
+				content += '1';
+			else
+				content += '0';
+			c <<= 1;
+		}
+		fin.read(&c, 1);
+	}
+
+	//Huffman编码解压文件
+	Node* p = root;
+	for (auto i : content)
+	{
+		if (p != NULL)
+		{
+			if (i == '0')
+				p = p->left;
+			else
+				p = p->right;
+			if (p != NULL)
+			{
+				if ((p->left == NULL) && (p->right == NULL))
+				{
+					fout.write(&p->c, 1);
+					--sum;
+					if (sum == 0)
+						break;
+					if (p != NULL)
+						p = root;
+				}
+			}
+		}
+	}
+}
+
+//解压函数
+void unzip(ifstream& fin, ofstream& fout)
+{
+	//读取原文件总字节数和各字符分别出现的次数
+	vector<int> count(256, 0);
+	int sum = readCount(fin, count);
+	//构造Huffman树的节点
+	vector<Node> nodes;
+	createNodes(nodes, count);
+	//构造Huffman树
+	Node* root = createTree(nodes);
+	//写入文件
+	uncompress(fin, fout, sum, root);
+	//释放Huffman树所占内存空间
+	deleteTree(root);
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 4)
+	{
+		cerr << "请确保输入参数个数正确" << endl;
+		return -1;
+	}
+
+	if (strcmp(argv[3], "zip") && strcmp(argv[3], "unzip"))
+	{
+		cerr << "未知参数!\n命令列表:\nzip\nunzip" << endl;
+		return -1;
+	}
+
+	ifstream fin(argv[1], ios::binary);
+	if (!fin)
+	{
+		cerr << "Can not open the input file!" << endl;
+		return -1;
+	}
+
+	ofstream fout(argv[2], ios::binary);
+	if (!fout)
+	{
+		cerr << "Can not open the output file!" << endl;
+		return -1;
+	}
+	if (strcmp(argv[3], "zip") == 0)
+		zip(fin, fout);
+	else if (strcmp(argv[3], "unzip") == 0)
+		unzip(fin, fout);
+	fin.close();
+	fout.close();
+	if (strcmp(argv[3], "zip") == 0)
+		cout << "成功压缩" << endl;
+	else if (strcmp(argv[3], "unzip") == 0)
+		cout << "成功解压" << endl;
+	return 0;
 }
---
+++ 输出时间程序改错/输出时间程序改错.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+class Time
+{
+	//错误1：成员函数需public，否则外部无法访问
+public:
+
+	void set_time(void);
+	void show_time(void);
+	//错误2：数据成员需为private保持封装
+private:
+
+	int hour;
+	int minute;
+	int sec;
+};
+
+Time t;
+
+int main()
+{
+	//错误3：外部调用成员函数需要加类名
+	//set_time();
+	//show_time();
+
+	t.set_time();
+	t.show_time();
+
+	return 0;
+}
+
+//错误4：外部定义成员函数需要给出类的作用域限定符,且返回类型不匹配
+//int set_time(void)
+void Time::set_time(void)
+
+{
+	//增加1：输入提示
+	cout << "请输入时，分，秒" << endl;
+
+	cin >> t.hour;
+	cin >> t.minute;
+	cin >> t.sec;
+}
+
+//错误5：外部定义成员函数需要给出类的作用域限定符,且返回类型不匹配
+//int show_time(void)
+void Time::show_time(void)
+
+{
+	//增加2：输出提示
+	cout << "你输入的时间是";
+
+	cout << t.hour << ":" << t.minute << ":" << t.sec << endl;
+}
